de0531cb791016c382435987fb21aa07175e7153
Jonathan O'Riordan <jooriordan@hotmail.com>
2024-01-25 11:11:23 +0000

    Created placeShip function and test
    The function checks the ships length and then uses a for loop to update the gameboard with the name of our ship

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,16 +1,33 @@
+import { Ship } from './ship';
 export class Gameboard {
     constructor() {
-
+      this.gameBoardArray = this.createGameboard();
     }
 
     createGameboard() {
         let arr = [];
+        let arrayItem = [];
         for (let i = 0; i < 10; i++) {
-            arr[i] = [];
             for (let j = 0; j < 10; j++) {
-              arr[i][j] = j;
+              arrayItem.push({ shipName: undefined});
             }
+            
+            arr.push(arrayItem);
+            console.log(arr)
+            arrayItem = [];
+            
           }
         return arr
     }
+
+    getGameBoard() {
+      return this.gameBoardArray;
+    }
+
+    placeShip(ship, x, y) {
+      for (let i = 0; i < ship.length; i++) {
+        this.gameBoardArray[y + i][x].shipName = ship;
+      }
+    }
+
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,7 +1,21 @@
 import { Gameboard } from '../gameboard';
+import { Ship } from '../ship';
 
-    const newGameboard = new Gameboard();
     test('to see if a 2d gameboard is produced by the for loop', () => {
+        const newGameboard = new Gameboard();
         expect(newGameboard.createGameboard().length).toBe(10);
         expect(newGameboard.createGameboard()[0].length).toBe(10);
     });
+
+    test('to check a ship has been placed at specific coordinates', () => {
+        const board = new Gameboard();
+        const admiral = new Ship(2);
+        let x = 1;
+        let y = 2;
+        board.placeShip(admiral, x, y);
+        expect(board.getGameBoard()[2][1]).toEqual({
+        shipName: admiral,
+        });
+    })
+
+    
