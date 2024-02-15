e605c8fe00ff04df7b4eae9be18001e943ab62e3
Jonathan O'Riordan <jooriordan@hotmail.com>
2024-02-15 10:26:20 +0000

    Loop for computer board created
    Loop which creates a 10 x 10 board of divs for the computer to match that of our own has been created
    Cell and class attributes also added for each div

--- src/dom.js
+++ src/dom.js
@@ -1,15 +1,32 @@
 import { Gameboard } from './gameboard';
 
 
-const createGameboard = (() => {
+export const createGameboard = (() => {
 for(let i = 0; i < 10; i++) {
+    const playerBoard = document.querySelector('.playerBoard');
+    const row = document.createElement('div');
+    row.classList.add('row-p1');
+    row.setAttribute("id", `p1-row${i}`);
+    playerBoard.appendChild(row);
     for (let j = 0; j < 10; j++) {
-        const playerBoard = document.querySelector('.playerBoard');
-        const boardSquares = document.createElement('div');
-        boardSquares.classList.add('row-p1');
-        row.setAttribute("id", `p1-row${i}`);
-        playerBoard.appendChild(boardSquares);
+        const cell = document.createElement('div');
+        cell.classList.add('cell-p1');
+        cell.setAttribute("id", `p1-cell${j}`);
+        row.appendChild(cell);
       }
     }
-    return { createGameboard };
-})()
+    for(let i = 0; i < 10; i++) {
+      const computerBoard = document.querySelector('.computerBoard');
+      const row = document.createElement('div');
+      row.classList.add('row-p2');
+      row.setAttribute("id", `p2-row${i}`);
+      computerBoard.appendChild(row);
+      for (let j = 0; j < 10; j++) {
+          const cell = document.createElement('div');
+          cell.classList.add('cell-p2');
+          cell.setAttribute("id", `p2-cell${j}`);
+          row.appendChild(cell);
+        }
+      }    
+
+})
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { createGameboard } from './dom';
+
+createGameboard();
